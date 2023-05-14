fc14d8f000620328ed7efcd17c012686edfe24f8
Darrell G Wolfe <darrellgwolfe@outlook.com>
2023-05-14 00:41:15 -0700

    Repair

--- Write_Query_Practice.sql
+++ Write_Query_Practice.sql
@@ -11,7 +11,7 @@ This failed, due to not having dates enclosed,
 Insert Into Employee
 (EmployeeId, FirstName, LastName,Aliases,DateOfBirth,Gender,Email,PhoneNumber,Address,City,State,PostalCode,HireDate, JobTitle,DepartmentId)
 Values
-(0000001,'Darrell','Wolfe','Storyteller',07/15/1980,'Male','darrellgwolfe@outlook.com','2083710283','24873 N Corbin Hill Rd','Athol','Idaho','83801',03/02/2022,'Chief Storyteller',1);
+(0000001,'Darrell','Wolfe','Storyteller',01/01/1969,'Male','test','testALSO','PO Box 1234','Athol','Idaho','83801',03/02/2022,'Chief Storyteller',1);
 */
 
 /*
@@ -19,7 +19,7 @@ Values
 INSERT INTO Employee
 (EmployeeId, FirstName, LastName, Aliases, DateOfBirth, Gender, Email, PhoneNumber, Address, City, State, PostalCode, HireDate, JobTitle, DepartmentId)
 VALUES
-('0000001', 'Darrell', 'Wolfe', 'Storyteller', '07/15/1980', 'Male', 'darrellgwolfe@outlook.com', '2083710283', '24873 N Corbin Hill Rd', 'Athol', 'Idaho', '83801', '03/02/2022', 'Chief Storyteller', 1);
+('0000001', 'Darrell', 'Wolfe', 'Storyteller', '01/01/1969', 'Male', 'test', 'testALSO', 'PO Box 1234', 'Athol', 'Idaho', '83801', '03/02/2022', 'Chief Storyteller', 1);
 Msg 544, Level 16, State 1, Line 19
 Cannot insert explicit value for identity column in table 'Employee' when IDENTITY_INSERT is set to OFF.
 */
@@ -29,7 +29,7 @@ Cannot insert explicit value for identity column in table 'Employee' when IDENTI
 INSERT INTO Employee
 (FirstName, LastName, Aliases, DateOfBirth, Gender, Email, PhoneNumber, Address, City, State, PostalCode, HireDate, JobTitle, DepartmentId)
 VALUES
-('Darrell', 'Wolfe', 'Storyteller', '07/15/1980', 'Male', 'darrellgwolfe@outlook.com', '2083710283', '24873 N Corbin Hill Rd', 'Athol', 'Idaho', '83801', '03/02/2022', 'Chief Storyteller', 1);
+('Darrell', 'Wolfe', 'Storyteller', '01/01/1969', 'Male', 'test', 'testALSO', 'PO Box 1234', 'Athol', 'Idaho', '83801', '03/02/2022', 'Chief Storyteller', 1);
 Msg 2628, Level 16, State 1, Line 28
 String or binary data would be truncated in table 'ToposCreativeDatabase.dbo.Employee', column 'Gender'. Truncated value: 'M'.
 The statement has been terminated.
@@ -40,7 +40,7 @@ The statement has been terminated.
 INSERT INTO Employee
 (FirstName, LastName, Aliases, DateOfBirth, Gender, Email, PhoneNumber, Address, City, State, PostalCode, HireDate, JobTitle, DepartmentId)
 VALUES
-('Darrell', 'Wolfe', 'Storyteller', '07/15/1980', 'M', 'darrellgwolfe@outlook.com', '2083710283', '24873 N Corbin Hill Rd', 'Athol', 'Idaho', '83801', '03/02/2022', 'Chief Storyteller', 1);
+('Darrell', 'Wolfe', 'Storyteller', '01/01/1969', 'M', 'test', 'testALSO', 'PO Box 1234', 'Athol', 'Idaho', '83801', '03/02/2022', 'Chief Storyteller', 1);
 
 Select *
 From Employee
@@ -55,6 +55,9 @@ Where EmployeeId=4
 Select *
 From Employee
 
+Update Employee
+Set Email = 'admin@darrellwolfe.com', PhoneNumber = '2089071617'
+Where EmployeeId = 4
 
 
 
